aa2222e6c2365742394a69e0367431961dbba8d4
TBS96 <9tbs6@proton.me>
2025-06-06 23:39:04 +0530

    feat: restructure App component to include QueryClientProvider and WeatherProvider
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
+import { WeatherProvider } from './context/WeatherContext'
+import Home from './pages/Home'
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <div>App</div>
+    <QueryClientProvider client={queryClient}>
+      <WeatherProvider>
+        <Home />
+      </WeatherProvider>
+    </QueryClientProvider>
   )
 }
 
