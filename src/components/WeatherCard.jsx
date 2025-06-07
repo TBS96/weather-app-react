34ccbc3714e670e5272609fdd356157a1a3277b3
TBS96 <9tbs6@proton.me>
2025-06-07 17:27:20 +0530

    feat: refactor logic for WeatherCard component with loading state management and error handling
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,10 +1,17 @@
 import { useWeatherContext } from '../context/WeatherContext'
 import { useQuery } from '@tanstack/react-query';
 import { fetchWeather } from '../api/weatherApi';
+import { useEffect, useRef, useState } from 'react';
 
 const WeatherCard = () => {
 
     const { city } = useWeatherContext();
+
+    const [showLoading, setShowLoading] = useState(true);
+
+    const loadingTimer = useRef(null);
+
+    const [haveData, setHaveData] = useState(false);
 
     const { data, error, isLoading } = useQuery({
         queryKey: ['weather', city],
@@ -14,7 +21,29 @@ const WeatherCard = () => {
 
     // console.log(data);
 
-    if (isLoading) return <div className='text-center mt-10'>Loading...</div>;
+    useEffect(() => {
+        setShowLoading(true);
+        setHaveData(false);
+
+        if (loadingTimer.current) {
+            clearTimeout(loadingTimer.current);
+        }
+
+        loadingTimer.current = setTimeout(() => {
+            setShowLoading(false);
+        }, 2000);
+
+        return () => {
+            if (loadingTimer.current) {
+                clearTimeout(loadingTimer.current);
+            }
+        }
+    }, [city]);
+
+    useEffect(() => {
+        if (data) setHaveData(true);
+    }, [data]);
+
     if (error) {
         return (
             <div role='alert' className='alert alert-error mt-10 w-fit mx-auto'>
@@ -23,7 +52,7 @@ const WeatherCard = () => {
         )
     }
 
-    if(!data) {
+    if (!data && !showLoading && !isLoading && haveData) {
         return (
             <div role='alert' className='alert alert-error mt-10 w-fit mx-auto'>
                 <span>Could not load weather data</span>
@@ -31,8 +60,13 @@ const WeatherCard = () => {
         )
     }
 
-    return (
-        <div className='card w-96 bg-base-100 shadow-xl mx-auto mt-10'>
+    return showLoading || isLoading ? (
+        <div className='grid place-items-center w-full min-h-[50vh]'>
+            <div className='loading loading-xl loading-infinity'></div>
+            <p className='text-center mt-4'>Loading Weather Data...</p>
+        </div>
+    ) : (
+        <div className='card w-96 bg-base-100 shadow-xl mx-auto mt-10 min-h-[50vh]'>
             <div className='card-body items-center text-center'>
                 <h2 className='card-title text-2xl'>{data.name}, {data.sys.country}</h2>
                 <p className='text-5xl'>{Math.round(data.main.temp)}° C</p>
