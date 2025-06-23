4b4b0b639c6b56e5131c264e3931fbc288f0c76c
TBS96 <9tbs6@proton.me>
2025-06-23 21:58:31 +0530

    feat: add PWA support and generate assets
    - Updated package.json to include PWA dependencies and scripts for generating PWA assets.
    - Added various PWA icons and a logo SVG to the public directory.
    - Configured Vite to use the PWA plugin with a manifest for the application.
    - Removed StrictMode wrapper from main.jsx for simplified rendering.
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // workbox: {
+      //   cleanupOutdatedCaches: true
+      // },
+      // devOptions: {
+      //   enabled: true
+      // },
+      manifest: {
+        name: 'Sun Syncs',
+        short_name: 'Sun Syncs',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#272525',
+        theme_color: '#272525',
+        scope: '/',
+        lang: 'en',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+        ],
+      },
+    }),
+  ],
 })
