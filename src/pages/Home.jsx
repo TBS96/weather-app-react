64d8471ab70b59c19742c1050b927f27fc34522e
TBS96 <9tbs6@proton.me>
2025-06-23 20:26:49 +0530

    feat: add tooltips to search input and button in Home component for better user guidance
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
             </a>
 
             <div className='flex flex-col sm:flex-row gap-4 sm:gap-2 mb-6'>
-                <label className='input input-info w-72'>
+                <label className='input input-info w-72 tooltip' data-tip='Search for a city or area pincode'>
                     <svg className='h-[1em] opacity-50' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
                         <g
                             strokeLinejoin='round'
@@ -47,7 +47,7 @@ const Home = () => {
                         onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
                     />
                 </label>
-                <button onClick={handleSearch} className='btn btn-primary sm:w-auto w-full'>
+                <button onClick={handleSearch} className='btn btn-primary sm:w-auto w-full tooltip tooltip-bottom sm:tooltip-top' data-tip='Search'>
                     Search
                 </button>
             </div>
