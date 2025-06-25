031dc2bd8037751208588863872ea2921e83ee05
TBS96 <9tbs6@proton.me>
2025-06-25 20:59:20 +0530

    feat: enhance weather API error handling and add city suggestion fetching
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -17,7 +17,35 @@ export const fetchWeatherByCoords = async (lat, lon) => {
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
     );
     if (!res.ok) {
-        throw new Error('City not found');
+        throw new Error('Coordinates are invalid or not found');
     }
     return res.json();
+};
+
+export const fetchCityBySuggestions = async (inputValue) => {
+    if (inputValue.trim().length < 2) {
+        return [];
+    }
+    try {
+        const res = await fetch(
+            `https://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=5&appid=${apiKey}`
+        );
+        if (!res.ok) {
+            console.error('Failed to fetch city suggestions from API');
+            return [];
+        }
+
+        const data = await res.json();
+        return data.map(({name, country, state, lat, lon}) => ({
+            name: name,
+            country: country,
+            state: state,
+            lat: lat,
+            lon: lon
+        }));
+    }
+    catch (err) {
+        console.error(`Error in fetchCitySuggestions: ${err}`);
+        return [];
+    }
 };
